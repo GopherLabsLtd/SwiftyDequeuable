5d617dfc13eef3d16fee07369ed5cbb84b151d4f
Alex Nguyen <alexnguyen@live.ca>
2017-08-19 16:28:54 -0400

    Initial commit

---
+++ SwiftyDequeuable/SwiftyDequeuable.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDequeuable.h
+//  SwiftyDequeuable
+//
+//  Created by Alex Nguyen on 2017-08-19.
+//  Copyright © 2017 Gopher Labs LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDequeuable.
+FOUNDATION_EXPORT double SwiftyDequeuableVersionNumber;
+
+//! Project version string for SwiftyDequeuable.
+FOUNDATION_EXPORT const unsigned char SwiftyDequeuableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDequeuable/PublicHeader.h>
+
+
